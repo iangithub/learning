f56e131324923d32bf4082d3dba265cdcd51a55f
Ian Chen <ian.msmvp@outlook.com>
2018-10-25 17:12:39 +0800

    init

---
+++ JsonSample/JsonSample/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            using (var request = new HttpRequestMessage())
+            {
+                request.Method = HttpMethod.Get;
+                request.RequestUri = new Uri("http://data.ntpc.gov.tw/api/v1/rest/datastore/382000000A-000352-001");
+                var response = await client.SendAsync(request);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                UbikeOpenDatas tranresult = JsonConvert.DeserializeObject<UbikeOpenDatas>(responseBody);
+
+                foreach (var item in tranresult.result.records)
+                {
+                    Console.WriteLine($"站點：{item.sna} / 可借車位數：{item.sbi} / 可還空位數：{item.bemp}");
+                }
+            }
+        }
+    }
+}
---
+++ JsonSample/JsonSample/jsonobj.cs
@@ -0,0 +1,41 @@
+﻿namespace JsonSample
+{
+    public class UbikeOpenDatas
+    {
+        public bool success { get; set; }
+        public DataResult result { get; set; }
+    }
+
+    public class DataResult
+    {
+        public string resource_id { get; set; }
+        public int limit { get; set; }
+        public int total { get; set; }
+        public FieldType[] fields { get; set; }
+        public BikeRecord[] records { get; set; }
+    }
+
+    public class FieldType
+    {
+        public string type { get; set; }
+        public string id { get; set; }
+    }
+
+    public class BikeRecord
+    {
+        public string sno { get; set; }
+        public string sna { get; set; }
+        public string tot { get; set; }
+        public string sbi { get; set; }
+        public string sarea { get; set; }
+        public string mday { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string ar { get; set; }
+        public string sareaen { get; set; }
+        public string snaen { get; set; }
+        public string aren { get; set; }
+        public string bemp { get; set; }
+        public string act { get; set; }
+    }
+}
